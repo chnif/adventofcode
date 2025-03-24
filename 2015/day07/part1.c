2375414b7235413d7d0bdee838513a2bc72b6357
chnif <chirif.ousri@gmail.com>
2025-03-24 16:40:07 +0100

    day 07 to do in the next days

---
+++ 2015/day07/part1.c
@@ -0,0 +1,26 @@
+// advent of code 2015
+// day XX : 
+// part 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include "../utils/utils.h"
+
+int solution()
+{
+    return 0;
+}
+
+int main()
+{
+	//char **lines = NULL;
+	//int lines_count = 1;
+	//int *count = NULL;
+	//read_file_to_array("input.txt", &lines, &count, &lines_count);
+	int x = 0;
+	printf("year 2015 day07 part 1 : %d \n", x);
+
+	return 0;
+}
